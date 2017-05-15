16842092730346b3e7a0898180c7e06adcb9d9f5
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2017-05-15 15:21:52 +0200

    Creating an "empty" app which runs but does nothing

---
+++ src/main/java/com/norton/exchange/ExchangeRateServiceApplication.java
@@ -0,0 +1,12 @@
+package com.norton.exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/norton/exchange/ExchangeRateServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.norton.exchange;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExchangeRateServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
