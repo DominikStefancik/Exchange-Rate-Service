97279b81a1bf7af424320381cf1eafdbbdf62f30
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2017-05-16 11:51:23 +0200

    The calling of GET request from RestTemplate is not in the try-catch block.

--- src/main/java/com/norton/exchange/rest/client/CurrencyUploader.java
+++ src/main/java/com/norton/exchange/rest/client/CurrencyUploader.java
@@ -2,9 +2,7 @@ package com.norton.exchange.rest.client;
 
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.norton.exchange.currency.Currency;
@@ -18,14 +16,9 @@ public class CurrencyUploader {
 
 		Map<String, List<Currency>> currenciesList;
 
-		try {
-			RestTemplate restTemplate = new RestTemplate();
-			String result = restTemplate.getForObject(ECB_URL, String.class);
-			currenciesList = CurrencyConverter.getCurrencyList(result);
-		} catch (RestClientException rcex) {
-			rcex.printStackTrace();
-			currenciesList = new TreeMap<>();
-		}
+		RestTemplate restTemplate = new RestTemplate();
+		String result = restTemplate.getForObject(ECB_URL, String.class);
+		currenciesList = CurrencyConverter.getCurrencyList(result);
 
 		return currenciesList;
 	}
